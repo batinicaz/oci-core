df81dba6e07e92cddb2765e6a8a9735ff3c87742
batinica <batinicaz@users.noreply.github.com>
2023-08-05 20:07:32 +0100

    fix: create security lists for public + private and attach to subnets

--- modules/vcn/security_list.tf
+++ modules/vcn/security_list.tf
@@ -1,21 +1,52 @@
-resource "oci_core_security_list" "allow_lb_to_instance" {
+resource "oci_core_security_list" "public" {
+  // checkov:skip=CKV_OCI_19: SSH required for packer builds
+
   compartment_id = var.compartment_id
-  display_name   = "internal-public-to-private"
   vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "public-security-list"
 
   defined_tags = merge(var.tags, {
-    "${var.tag_namespace}.name" = "internal-public-to-private"
+    "${var.tag_namespace}.name" = "public-security-list"
   })
 
-  dynamic "egress_security_rules" {
-    for_each = var.private_subnets
-    content {
-      description      = "Allow internal traffic to private subnets"
-      destination      = egress_security_rules.value
-      destination_type = "CIDR_BLOCK"
-      protocol         = "6" // TCP
-      stateless        = false
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
+
+  // HTTPS
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+    tcp_options {
+      min = 443
+      max = 443
     }
+  }
+
+  // SSH for packer builds
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+}
+
+resource "oci_core_security_list" "private" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "private-security-list"
+
+  defined_tags = merge(var.tags, {
+    "${var.tag_namespace}.name" = "private-security-list"
+  })
+
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
   }
 
   dynamic "ingress_security_rules" {
