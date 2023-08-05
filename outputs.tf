618fd0f84b1541f91fd1dcbaef40c6566c0957bc
batinica <batinicaz@users.noreply.github.com>
2023-08-05 10:15:08 +0100

    feat: add kms vault

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "core_vcn_subnets" {
   value = merge(module.core_vcn.public_subnets, module.core_vcn.private_subnets)
 }
 
+output "kms_vault_endpoint" {
+  value = oci_kms_vault.terraform.management_endpoint
+}
+
 output "terraform_identity_compartment_id" {
   value = oci_identity_compartment.terraform.id
 }
